493bf94f0651d58dd7143c268e5a379044163915
dontNedd <datafootprint@protonmail.com>
2025-07-18 05:18:24 +0000

    kickstart_myconfig

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,23 @@
--- Sync clipboard between OS and Neovim.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-require("aos.config.lazy")
-require("aos.config.keybinds")
-require("aos.config.options")
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
