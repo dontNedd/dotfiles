b6911e6143147e72026d4d8ab72787243aa83d32
dontNedd <datafootprint@protonmail.com>
2025-07-18 02:05:56 +0000

    bad previous push

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- Sync clipboard between OS and Neovim.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
-
-require("aos.config.lazy")
-require("aos.config.keybinds")
-require("aos.config.options")
